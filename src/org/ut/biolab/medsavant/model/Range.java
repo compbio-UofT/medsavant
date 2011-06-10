fb17329681e13f2daeb7b801a9753871fb40b77a
Nirvana Nursimulu <nnursimulu@cs.toronto.edu>
2011-06-10 19:18:12 +0000

    Added some methods to allow merging with ranges.  Made min and max private, and added getter methods for them.
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/trunk/MedSavant@1710 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- src/org/ut/biolab/medsavant/model/Range.java
+++ src/org/ut/biolab/medsavant/model/Range.java
@@ -1,5 +1,9 @@
 package org.ut.biolab.medsavant.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -9,13 +13,84 @@ package org.ut.biolab.medsavant.model;
  *
  * @author mfiume
  */
-public class Range {
+public class Range implements Comparable{
     
-    public final double min;
-    public final double max;
+    private double min;
+    private double max;
 
     public Range(double min, double max) {
         this.min = min;
         this.max = max;
     }
+    
+    
+    public double getMin(){
+        return min;
+    }
+    
+    public double getMax(){
+        return max;
+    }
+    
+    /**
+     * Return the list of ranges which result from merging the ranges from 
+     * range1 with those from range2.
+     * @param range1
+     * @param range2
+     * @return 
+     */
+    public List<Range> merge(List<Range> range1, List<Range> range2){
+        
+        List<Range> mergedList = new ArrayList<Range>();
+        // Arrange first in order by starting positions.
+        TreeSet<Range> allRangesInOrder = new TreeSet<Range>();
+        range1.addAll(range2);
+        for (Range currRange:range1){
+            allRangesInOrder.add(currRange);
+        }
+        // Merge now.
+        Range currMerged = null;
+
+        for (Range currRange: allRangesInOrder){
+            if (currMerged == null){
+                currMerged = currRange;
+                mergedList.add(currMerged);
+            }
+            else if (currMerged.canBeMergedWith(currRange)){
+                // merge them here
+                currMerged.max = Math.max(currMerged.max, currRange.max);
+            }
+            else{
+                currMerged = currRange;
+                mergedList.add(currMerged);
+            }
+        }
+        return mergedList;
+    }
+
+    /**
+     * Compare two Range objects by starting point.
+     * @param rangeObj
+     * @return 
+     */
+    public int compareTo(Object rangeObj) {
+        Range range = (Range) rangeObj;
+        if (this.min < range.min)
+            return -1;
+        else if (this.min > range.min)
+            return 1;
+        else
+            return 0;
+    }
+    
+    /**
+     * Returns true iff this range and the range given intersect.
+     * @param range
+     * @return 
+     */
+    public boolean canBeMergedWith(Range range){
+        return (this.min <= range.max && range.max <= this.max) || 
+                (range.min <= this.max && this.max <= range.max);
+    }
+    
 }
