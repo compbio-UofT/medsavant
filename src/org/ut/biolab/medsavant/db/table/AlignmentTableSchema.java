e55acf10b2b09207d9a937c08accc5ef7b113e1d
Andrew Brook <abrook@@cs.toronto.edu>
2011-07-05 18:53:11 +0000

    alignment table schema
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/trunk@1923 096565f4-0d05-4fcd-8cbf-ff6523290f6f

---
+++ src/org/ut/biolab/medsavant/db/table/AlignmentTableSchema.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ut.biolab.medsavant.db.table;
+
+import com.healthmarketscience.sqlbuilder.dbspec.basic.DbSchema;
+
+/**
+ *
+ * @author AndrewBrook
+ */
+public class AlignmentTableSchema extends TableSchema {
+    
+    public static final String TABLE_NAME = "alignment";
+   
+    public static int INDEX_DNAID;
+    public static int INDEX_PIPELINEID;
+    public static int INDEX_ALIGNMENTPATH;
+    
+    public static final String ALIAS_DNAID = "DNA ID";
+    public static final String ALIAS_PIPELINEID = "Pipeline ID";
+    public static final String ALIAS_ALIGNMENTPATH = "Alignment Path";
+
+    public static final String DBFIELDNAME_DNAID = "dna_id";
+    public static final String DBFIELDNAME_PIPELINEID = "pipeline_id";
+    public static final String DBFIELDNAME_ALIGNMENTPATH = "alignment_path";
+
+    public AlignmentTableSchema(DbSchema s) {
+        super(s.addTable(TABLE_NAME));
+        addColumns();
+        setIndexes();
+    }
+    
+    private void setIndexes() {
+        INDEX_DNAID = this.getFieldIndexInDB(DBFIELDNAME_DNAID);
+        INDEX_PIPELINEID = this.getFieldIndexInDB(DBFIELDNAME_PIPELINEID);
+        INDEX_ALIGNMENTPATH = this.getFieldIndexInDB(DBFIELDNAME_ALIGNMENTPATH);
+    }
+ 
+    private void addColumns() {
+        addColumn(DBFIELDNAME_DNAID,ALIAS_DNAID,TableSchema.ColumnType.VARCHAR,40);
+        addColumn(DBFIELDNAME_PIPELINEID,ALIAS_PIPELINEID,TableSchema.ColumnType.INTEGER,11);
+        addColumn(DBFIELDNAME_ALIGNMENTPATH,ALIAS_ALIGNMENTPATH,TableSchema.ColumnType.VARCHAR,200);
+    }
+    
+}
--- src/org/ut/biolab/medsavant/db/table/CohortTableSchema.java
+++ src/org/ut/biolab/medsavant/db/table/CohortTableSchema.java
@@ -16,11 +16,12 @@ public class CohortTableSchema extends TableSchema {
    
     public static int INDEX_COHORTID;
     public static int INDEX_COHORTNAME;
+    
+    public static final String ALIAS_COHORTID = "Cohort ID";
+    public static final String ALIAS_COHORTNAME = "Cohort Name";
 
-    private void setIndexes() {
-        INDEX_COHORTID = this.getFieldIndexInDB(DBFIELDNAME_COHORTID);
-        INDEX_COHORTNAME = this.getFieldIndexInDB(DBFIELDNAME_COHORTNAME);
-    }
+    public static final String DBFIELDNAME_COHORTID = "id";
+    public static final String DBFIELDNAME_COHORTNAME = "name";
 
     public CohortTableSchema(DbSchema s) {
         super(s.addTable(TABLE_NAME));
@@ -28,13 +29,10 @@ public class CohortTableSchema extends TableSchema {
         setIndexes();
     }
    
-    
-    public static final String ALIAS_COHORTID = "Cohort ID";
-    public static final String ALIAS_COHORTNAME = "Cohort Name";
-
-    
-    public static final String DBFIELDNAME_COHORTID = "id";
-    public static final String DBFIELDNAME_COHORTNAME = "name";
+    private void setIndexes() {
+        INDEX_COHORTID = this.getFieldIndexInDB(DBFIELDNAME_COHORTID);
+        INDEX_COHORTNAME = this.getFieldIndexInDB(DBFIELDNAME_COHORTNAME);
+    }
  
     private void addColumns() {
         addColumn(DBFIELDNAME_COHORTID,ALIAS_COHORTID,TableSchema.ColumnType.INTEGER,11);
