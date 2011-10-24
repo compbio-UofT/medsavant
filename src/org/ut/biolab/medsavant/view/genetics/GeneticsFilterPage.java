e9e8d34fdda10146ba4d946d6538e3566fdc7e93
Marc Fiume <mfiume@cs.toronto.edu>
2011-10-24 15:34:13 +0000

    Experimental view of SQL statement
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/branches/b4/MedSavant@2605 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- src/org/ut/biolab/medsavant/view/genetics/GeneticsFilterPage.java
+++ src/org/ut/biolab/medsavant/view/genetics/GeneticsFilterPage.java
@@ -4,19 +4,62 @@
  */
 package org.ut.biolab.medsavant.view.genetics;
 
+import com.healthmarketscience.sqlbuilder.Condition;
+import com.healthmarketscience.sqlbuilder.FunctionCall;
+import com.healthmarketscience.sqlbuilder.SelectQuery;
 import java.awt.BorderLayout;
+import java.sql.SQLException;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import org.ut.biolab.medsavant.controller.FilterController;
+import org.ut.biolab.medsavant.db.exception.FatalDatabaseException;
+import org.ut.biolab.medsavant.db.exception.NonFatalDatabaseException;
+import org.ut.biolab.medsavant.db.model.structure.CustomTables;
+import org.ut.biolab.medsavant.db.model.structure.TableSchema;
+import org.ut.biolab.medsavant.db.util.query.VariantQueryUtil;
+import org.ut.biolab.medsavant.model.event.FiltersChangedListener;
 import org.ut.biolab.medsavant.view.util.PeekingPanel;
 import org.ut.biolab.medsavant.view.genetics.filter.FilterPanel;
 import org.ut.biolab.medsavant.view.genetics.filter.FilterProgressPanel;
 import org.ut.biolab.medsavant.view.subview.SectionView;
 import org.ut.biolab.medsavant.view.subview.SubSectionView;
+import org.ut.biolab.medsavant.view.util.ViewUtil;
 
 /**
  *
  * @author mfiume
  */
 public class GeneticsFilterPage extends SubSectionView {
+
+    private static class FilterSQLPanel extends JPanel implements FiltersChangedListener {
+        private final JTextArea content;
+
+        public FilterSQLPanel() {
+            this.setBorder(ViewUtil.getBigBorder());
+            content = new JTextArea();
+            content.setEditable(false);
+            this.setLayout(new BorderLayout());
+            this.add(new JScrollPane(content));
+            updateSQL();
+            FilterController.addFilterListener(this);
+        }
+
+        public void filtersChanged() throws SQLException, FatalDatabaseException, NonFatalDatabaseException {
+            updateSQL();
+        }
+
+        private void updateSQL() {
+            Condition[][] conditions = FilterController.getQueryFilterConditions();
+             TableSchema table = CustomTables.getVariantTableSchema("VARIANT TABLE");
+               
+            SelectQuery q = new SelectQuery();
+            //q.addFromTable(table.getTable());
+            VariantQueryUtil.addConditionsToQuery(q, conditions);
+            
+            this.content.setText(q.toString());
+        }
+    }
 
     private JPanel view;
     private FilterPanel fp;
@@ -35,6 +78,7 @@ public class GeneticsFilterPage extends SubSectionView {
             view.setLayout(new BorderLayout());
             fp = new FilterPanel();
             view.add(fp,BorderLayout.CENTER);
+            view.add(new PeekingPanel("SQL Statement", BorderLayout.NORTH, new FilterSQLPanel(),false,150),BorderLayout.SOUTH);
             view.add(new PeekingPanel("History", BorderLayout.WEST, new FilterProgressPanel(), true), BorderLayout.EAST);
         } else {
             fp.refreshSubPanels();
