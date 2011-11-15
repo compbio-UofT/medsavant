a0296262e121e0f93ad4385b3a8e9291d79b9502
Marc Fiume <mfiume@cs.toronto.edu>
2011-11-15 22:37:26 +0000

    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/trunk@2734 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- 1.0.1-nov15-2011/src/org/ut/biolab/medsavant/listener/ProjectListener.java
+++
@@ -1,9 +0,0 @@
-package org.ut.biolab.medsavant.listener;
-
-public interface ProjectListener {
-        public void projectAdded(String projectName);
-        public void projectRemoved(String projectName);
-        public void projectChanged(String projectName);
-        public void projectTableRemoved(int projid, int refid);
-}
-   
