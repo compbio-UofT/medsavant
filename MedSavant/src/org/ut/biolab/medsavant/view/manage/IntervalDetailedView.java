55d5e8fa930e018d3feedd262a14673101e7c9df
Andrew Brook <abrook@@cs.toronto.edu>
2011-12-08 21:16:53 +0000

    Filter from region page
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/trunk@2868 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- MedSavant/src/org/ut/biolab/medsavant/view/manage/IntervalDetailedView.java
+++ MedSavant/src/org/ut/biolab/medsavant/view/manage/IntervalDetailedView.java
@@ -4,7 +4,8 @@
  */
 package org.ut.biolab.medsavant.view.manage;
 
-import com.jidesoft.utils.SwingWorker;
+import com.healthmarketscience.sqlbuilder.ComboCondition;
+import com.healthmarketscience.sqlbuilder.Condition;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
@@ -12,20 +13,28 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Vector;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JOptionPane;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
+import org.ut.biolab.medsavant.controller.ProjectController;
+import org.ut.biolab.medsavant.db.api.MedSavantDatabase.DefaultVariantTableSchema;
+import org.ut.biolab.medsavant.db.model.GenomicRegion;
+import org.ut.biolab.medsavant.db.model.Range;
+import org.ut.biolab.medsavant.db.model.RangeCondition;
 import org.ut.biolab.medsavant.db.model.RegionSet;
+import org.ut.biolab.medsavant.db.util.BinaryConditionMS;
 import org.ut.biolab.medsavant.db.util.query.RegionQueryUtil;
 import org.ut.biolab.medsavant.util.MedSavantWorker;
+import org.ut.biolab.medsavant.view.genetics.filter.FilterPanelSubItem;
+import org.ut.biolab.medsavant.view.genetics.filter.FilterUtils;
 import org.ut.biolab.medsavant.view.list.DetailedView;
 import org.ut.biolab.medsavant.view.util.ViewUtil;
 
@@ -40,9 +49,9 @@ public class IntervalDetailedView extends DetailedView {
     private RegionDetailsSW sw;
     private final JPanel content;
     private final JPanel details;
-
     private int numRegionsInRegionList;
     private RegionSet regionSet;
+    private static List<FilterPanelSubItem> filterPanels;
 
     @Override
     public void setMultipleSelections(List<Object[]> selectedRows) {
@@ -59,7 +68,8 @@ public class IntervalDetailedView extends DetailedView {
 
     @Override
     public void setRightClick(MouseEvent e) {
-        //nothing yet
+        JPopupMenu popup = createPopup(regionSet);
+        popup.show(e.getComponent(), e.getX(), e.getY()); 
     }
 
     /*
@@ -192,6 +202,77 @@ public class IntervalDetailedView extends DetailedView {
         }
         sw = new RegionDetailsSW(regionList,limitNumberOfRegionsShown);
         sw.execute();
+    }
+    
+    private JPopupMenu createPopup(final RegionSet set){
+        JPopupMenu popupMenu = new JPopupMenu();
+        
+        if(ProjectController.getInstance().getCurrentVariantTableSchema() == null){
+            popupMenu.add(new JLabel("(You must choose a variant table before filtering)"));
+        } else {
+
+            //Filter by patient
+            JMenuItem filter1Item = new JMenuItem("Filter by Region List");
+            filter1Item.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                    
+                    try {
+                        List<GenomicRegion> regions = RegionQueryUtil.getRegionsInRegionSet(set.getId());
+                        Map<String, List<Range>> rangeMap = GenomicRegion.mergeGenomicRegions(regions);
+                        Condition[] results = new Condition[rangeMap.size()];
+                        int i = 0;
+                        for(String chrom : rangeMap.keySet()){
+
+                            Condition[] tmp = new Condition[2];
+
+                            //add chrom condition
+                            tmp[0] = BinaryConditionMS.equalTo(
+                                    ProjectController.getInstance().getCurrentVariantTableSchema().getDBColumn(DefaultVariantTableSchema.COLUMNNAME_OF_CHROM), 
+                                    chrom);
+
+                            //create range conditions
+                            List<Range> ranges = rangeMap.get(chrom);
+                            Condition[] rangeConditions = new Condition[ranges.size()];
+                            for(int j = 0; j < ranges.size(); j++){
+                                rangeConditions[j] = new RangeCondition(
+                                        ProjectController.getInstance().getCurrentVariantTableSchema().getDBColumn(DefaultVariantTableSchema.COLUMNNAME_OF_POSITION), 
+                                        (long)ranges.get(j).getMin(), 
+                                        (long)ranges.get(j).getMax());
+                            }
+
+                            //add range conditions
+                            tmp[1] = ComboCondition.or(rangeConditions);
+
+                            results[i] = ComboCondition.and(tmp);
+
+                            i++;
+                        }
+                        
+                        removeExistingFilters();
+                        filterPanels = FilterUtils.createAndApplyGenericFixedFilter(
+                                "Region Lists - Filter by List", 
+                                regions.size() + " Region(s)", 
+                                ComboCondition.or(results));
+       
+                    } catch (SQLException ex) {
+                        Logger.getLogger(IntervalDetailedView.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                }
+            });
+            popupMenu.add(filter1Item);          
+        }
+
+        return popupMenu;
+    }
+    
+    private void removeExistingFilters(){
+        if(filterPanels != null){
+            for(FilterPanelSubItem panel : filterPanels){
+                panel.removeThis();
+            }
+        }
     }
 
 }
