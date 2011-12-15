14698afeb75b5df53389974a1b3c5151fdba4712
Marc Fiume <mfiume@cs.toronto.edu>
2011-12-15 19:21:53 +0000

    Upload variants in parts
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/trunk@2894 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- VCF/src/org/ut/biolab/medsavant/vcf/VCFParser.java
+++ VCF/src/org/ut/biolab/medsavant/vcf/VCFParser.java
@@ -2,11 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.ut.biolab.medsavant.vcf;
 
 import au.com.bytecode.opencsv.CSVReader;
 import java.io.BufferedWriter;
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -20,51 +20,108 @@ import java.util.List;
  * @author mfiume
  */
 public class VCFParser {
-    private static char defaultDelimiter = '\t';
-    private static String headerChars = "#";
-    private static String commentSplitter = "=";
-    private static String commentChars = "##";
 
-    private static void parseVariantsFromReader(CSVReader r, File outfile, int updateId, int fileId) throws IOException {
-        
+    public static final char defaultDelimiter = '\t';
+    public static final String headerChars = "#";
+    public static final String commentSplitter = "=";
+    public static final String commentChars = "##";
+
+    public static VCFHeader parseVCFHeader(CSVReader r) throws IOException {
+        String[] nextLine;
+        while (true) {
+            if ((nextLine = r.readNext()) == null) {
+                break;
+            }
+            // a comment line
+            if (nextLine[0].startsWith(commentChars)) {
+                //do nothing
+            } // header line
+            else if (nextLine[0].startsWith(headerChars)) {
+                return parseHeader(nextLine);
+            } // a data line
+        }
+
+        return null;
+    }
+
+    public static boolean parseVariantsFromReader(CSVReader r, int outputLinesLimit, File outfile, int updateId, int fileId) throws IOException {
+        return parseVariantsFromReader(r, null, outputLinesLimit, outfile, updateId, fileId);
+    }
+
+    /**
+     * Like parseVariantsFromReader, but use a pre-parsed header object (useful when
+     * reusing a header)
+     * @param r A reader for .vcf file being parsed
+     * @param header The VCF header object to use. If header == null, the first header line in the file will be used
+     * @param outputLinesLimit The number of lines to write to output before returning
+     * @param outfile The temporary output file, with variants parsed 1 per position per individual
+     * @param updateId The updateId to prepend to each line
+     * @param fileId The fileId to prepend to each line
+     * @return Whether or not the function quit early (as per outputLinesLimit) or finished the file
+     * @throws IOException
+     */
+    public static boolean parseVariantsFromReader(CSVReader r, VCFHeader header, int outputLinesLimit, File outfile, int updateId, int fileId) throws IOException {
+
+        System.out.println("Starting to parse variants from reader");
+
         String[] nextLine;
         int numRecords = 0;
-        VCFHeader header = null;
-        
-        try {
-            BufferedWriter out = new BufferedWriter(new FileWriter(outfile, false));
-            
-            int variantId = 0;
-            while((nextLine = r.readNext()) != null) {
-                // a comment line
-                if (nextLine[0].startsWith(commentChars)) {
-                    //do nothing
-                }
-                // header line
-                else if (nextLine[0].startsWith(headerChars)) {
+
+        boolean didParseCompleteFile = false;
+
+        BufferedWriter out = new BufferedWriter(new FileWriter(outfile, false));
+
+        int variantId = 0;
+        int numLinesWritten = 0;
+
+        while (true) {
+            if (numLinesWritten > outputLinesLimit) {
+                break;
+            }
+            if ((nextLine = r.readNext()) == null) {
+                didParseCompleteFile = true;
+                break;
+            }
+
+            // a comment line
+            if (nextLine[0].startsWith(commentChars)) {
+                //do nothing
+            } // header line
+            else if (nextLine[0].startsWith(headerChars)) {
+                if (header == null) {
                     header = parseHeader(nextLine);
                 }
-                // a data line
-                else {
-                    List<VariantRecord> records = parseRecord(nextLine,header);
-                    //add records to tdf
-                    for(VariantRecord v : records){
-                        out.write(v.toTabString(updateId, fileId, variantId));
-                        out.write("\r\n");
-                        variantId++;
-                    }
-                    numRecords++;
+            } // a data line
+            else {
+                List<VariantRecord> records = null;
+                try {
+                    records = parseRecord(nextLine, header);
+                } catch (NullPointerException e) {
+                    System.out.println("Next line: " + nextLine);
+                    throw e;
                 }
+                //add records to tdf
+                for (VariantRecord v : records) {
+                    out.write(v.toTabString(updateId, fileId, variantId));
+                    numLinesWritten++;
+                    out.write("\r\n");
+                    variantId++;
+                }
+                numRecords++;
             }
-            out.close();
-        } catch (IOException ex){
-            ex.printStackTrace();
         }
+        out.close();
+
+        System.out.println("Read " + numRecords + " lines");
+
+
+        return didParseCompleteFile;
     }
-    
-    public static void parseVariants(File vcffile, File outfile, char delimiter, int updateId, int fileId) throws FileNotFoundException, IOException{
+
+    public static void parseVariants(File vcffile, File outfile, char delimiter, int updateId, int fileId) throws FileNotFoundException, IOException {
         CSVReader r = openFile(vcffile, delimiter);
-        parseVariantsFromReader(r, outfile, updateId, fileId);
+        parseVariantsFromReader(r, Integer.MAX_VALUE, outfile, updateId, fileId);
+        r.close();
     }
 
     private static CSVReader openFile(File vcffile, char delim) throws FileNotFoundException {
@@ -79,7 +136,7 @@ public class VCFParser {
         if (headerLine.length > VCFHeader.getNumMandatoryFields()) {
 
             // get the genotype labels
-            for (int i = VCFHeader.getNumMandatoryFields()+1; i < headerLine.length; i++) {
+            for (int i = VCFHeader.getNumMandatoryFields() + 1; i < headerLine.length; i++) {
                 result.addGenotypeLabel(headerLine[i]);
             }
         }
@@ -91,21 +148,31 @@ public class VCFParser {
         commentLine = commentLine.replaceFirst(commentChars, "");
         int indexOfSplit = commentLine.indexOf(commentSplitter);
         String[] result = new String[2];
-        result[0] = commentLine.substring(0,indexOfSplit);
-        result[1] = commentLine.substring(indexOfSplit+1);
+        result[0] = commentLine.substring(0, indexOfSplit);
+        result[1] = commentLine.substring(indexOfSplit + 1);
         return result;
     }
-    
+
     public static void parseVariants(File vcffile, File outfile, int updateId, int fileId) throws FileNotFoundException, IOException {
         parseVariants(vcffile, outfile, defaultDelimiter, updateId, fileId);
     }
-      
+
     private static List<VariantRecord> parseRecord(String[] line, VCFHeader h) {
         int numMandatoryFields = VCFHeader.getNumMandatoryFields();
 
+
+        /**
+         * IGNORE LARGE DELETIONS
+         */
+        if (line[4].equals("<DEL>")) {
+            System.err.println("WARNING: Ignoring large deletion at " + line[0] + ":" + line[1]);
+            return new ArrayList<VariantRecord>();
+        }
+
+
         List<String> infos = new ArrayList<String>();
         List<String> ids;
-        
+
         for (int i = numMandatoryFields; i < line.length; i++) {
             infos.add(line[i]);
         }
@@ -114,26 +181,28 @@ public class VCFParser {
             infos.add(".");
             ids = new ArrayList<String>();
             ids.add(".");
-        }
-        else {
+        } else {
             ids = h.getGenotypeLabels();
         }
-        
+
         //GenotypeField[] formatHeader = parseFormat(line[numMandatoryFields]);
-        
+
         List<VariantRecord> records = new ArrayList<VariantRecord>();
-        VariantRecord r = new VariantRecord(line);
+
+        VariantRecord r = null;
+        try {
+            r = new VariantRecord(line);
+        } catch (Exception e) {
+            System.err.println("WARNING: error parsing line " + line + ". Skipping");
+            return new ArrayList<VariantRecord>();
+        }
         for (int i = 0; i < ids.size(); i++) {
             String id = ids.get(i);
             VariantRecord r2 = new VariantRecord(r);
             r2.setDnaID(id);
-            //r2.setGenotypeFields(formatHeader, line[numMandatoryFields+1+i]);
-            //r2.setCallDetails(info);
             records.add(r2);
-            //Logger.log(VCFParser.class, "Read " + r2.toString());
         }
-        
+
         return records;
     }
-
 }
