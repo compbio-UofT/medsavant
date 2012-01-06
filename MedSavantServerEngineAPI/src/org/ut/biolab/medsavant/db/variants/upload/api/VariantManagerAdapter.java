8c66fe12f99bf2880c046c50a1c61ca154007de8
Marc Fiume <mfiume@cs.toronto.edu>
2012-01-06 20:25:56 +0000

    Server -> client communication
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/branches/1.0.1@2992 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- MedSavantServerEngineAPI/src/org/ut/biolab/medsavant/db/variants/upload/api/VariantManagerAdapter.java
+++ MedSavantServerEngineAPI/src/org/ut/biolab/medsavant/db/variants/upload/api/VariantManagerAdapter.java
@@ -4,17 +4,20 @@ import com.healthmarketscience.rmiio.RemoteInputStream;
 import java.io.IOException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.sql.SQLException;
 
 /**
  *
  * @author mfiume
  */
-public interface UploadVariantsAdapter extends Remote
+public interface VariantManagerAdapter extends Remote
 {
 
   public int uploadVariants(String sid, RemoteInputStream[] fileStreams, int projectId, int referenceId) throws RemoteException, IOException, Exception;
 
   public void annotateVariants(String sid, int projectID, int referenceID, int updateID) throws RemoteException;
+
+  public void publishVariants(String sid, int projectID, int referenceID, int updateID) throws RemoteException, SQLException;
 
 }
 
