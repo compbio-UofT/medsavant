b9179d361e6db710b59d9b2f2d6f628f892ec319
Andrew Brook <abrook@@cs.toronto.edu>
2012-01-17 19:17:31 +0000

    Add listeners to menu toolbar
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/branches/1.0.1@3038 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- MedSavantClient/src/org/ut/biolab/medsavant/view/menu/TopMenu.java
+++ MedSavantClient/src/org/ut/biolab/medsavant/view/menu/TopMenu.java
@@ -8,11 +8,21 @@ import com.jidesoft.swing.JideSplitButton;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.AbstractAction;
 import javax.swing.Box;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
+import org.ut.biolab.medsavant.controller.LoginController;
+import org.ut.biolab.medsavant.controller.ProjectController;
+import org.ut.biolab.medsavant.controller.ReferenceController;
+import org.ut.biolab.medsavant.listener.ProjectListener;
+import org.ut.biolab.medsavant.listener.ReferenceListener;
+import org.ut.biolab.medsavant.model.event.LoginEvent;
+import org.ut.biolab.medsavant.model.event.LoginListener;
 import org.ut.biolab.medsavant.view.ViewController;
+import org.ut.biolab.medsavant.view.genetics.GeneticsSection;
 import org.ut.biolab.medsavant.view.subview.SectionView;
 import org.ut.biolab.medsavant.view.subview.SubSectionView;
 
@@ -24,12 +34,44 @@ public class TopMenu extends JToolBar {
     
     private SubSectionView currentView;
     private final JPanel contentContainer;
+    private List<SubSectionView> subSectionViews = new ArrayList<SubSectionView>();
     
     public TopMenu(JPanel panel){
         super();
         this.setFloatable(false);
         
         contentContainer = panel;
+        
+        ReferenceController.getInstance().addReferenceListener(new ReferenceListener() {
+            public void referenceChanged(String referenceName) {
+                updateSections();
+            }
+            public void referenceAdded(String name) {}
+            public void referenceRemoved(String name) {}
+        });
+
+        ProjectController.getInstance().addProjectListener(new ProjectListener() {
+            public void projectAdded(String projectName) {}
+            public void projectRemoved(String projectName) {}
+            public void projectChanged(String projectName) {      
+                if(!GeneticsSection.isInitialized){ 
+                    //once this section is initialized, referencecombobox fires
+                    //referencechanged event on every project change
+                    updateSections();
+                }
+            }
+            public void projectTableRemoved(int projid, int refid) {}
+        });
+        
+        LoginController.addLoginListener(new LoginListener() {
+            public void loginEvent(LoginEvent evt) {
+                if (evt.getType() == LoginEvent.EventType.LOGGED_OUT) {
+                    contentContainer.removeAll();
+                    ViewController.getInstance().changeSubSectionTo(null);
+                    currentView = null;
+                }
+            }
+        });
     }
     
     public void addSection(SectionView section) {
@@ -39,6 +81,7 @@ public class TopMenu extends JToolBar {
         add(button);      
         
         for (final SubSectionView v : section.getSubSections()) {
+            subSectionViews.add(v);
             button.add(new AbstractAction(v.getName()) {
                 public void actionPerformed(ActionEvent e) {
                     setContentTo(v, false);
@@ -50,6 +93,15 @@ public class TopMenu extends JToolBar {
     public void addComponent(Component c) {
         add(c);
         add(Box.createHorizontalStrut(30));
+    }
+    
+    public void updateSections(){
+        for(int i = 0; i < subSectionViews.size(); i++) {
+            subSectionViews.get(i).setUpdateRequired(true);
+        }
+        if (currentView != null) {
+            setContentTo(currentView, true);
+        }    
     }
     
     private void setContentTo(SubSectionView v, boolean update) {
