07881c6f224646261c33fbd2b7d883d80a9c30c0
Marc Fiume <mfiume@cs.toronto.edu>
2012-02-04 20:01:53 +0000

    Updates to HPO
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/branches/1.0.1@3161 096565f4-0d05-4fcd-8cbf-ff6523290f6f

---
+++ MedSavantShared/src/org/ut/biolab/medsavant/db/api/MedSavantDatabaseExtras.java
@@ -0,0 +1,11 @@
+package org.ut.biolab.medsavant.db.api;
+
+/**
+ *
+ * @author mfiume
+ */
+public class MedSavantDatabaseExtras {
+
+    public static final String OPTIONAL_PATIENT_FIELD_HPO = "hpo";
+
+}
