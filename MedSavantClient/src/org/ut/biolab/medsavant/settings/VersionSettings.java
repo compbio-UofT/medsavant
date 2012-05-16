7d017f75ce935e70cd61b6ad84bacb384d273637
Eric Smith <eric.smith@utoronto.ca>
2012-05-16 19:16:12 +0000

    Updated version to 1.0.1 beta.
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/trunk@3488 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- MedSavantClient/src/org/ut/biolab/medsavant/settings/VersionSettings.java
+++ MedSavantClient/src/org/ut/biolab/medsavant/settings/VersionSettings.java
@@ -53,7 +53,7 @@ public class VersionSettings {
     public static final URL PLUGIN_URL = ClientNetworkUtils.getKnownGoodURL(URL, "plugins/plugin.xml");
     public static final URL LOG_USAGE_STATS_URL = ClientNetworkUtils.getKnownGoodURL(URL, "scripts/logUsageStats.cgi");
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static String BUILD = "beta";
 
     /**
