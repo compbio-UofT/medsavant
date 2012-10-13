a53a0908bec9b0d762f27250bd51bbbfc3672c78
Marc Fiume <mfiume@cs.toronto.edu>
2012-10-13 20:32:40 +0000

    Added "Family Matters" code
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/trunk@3710 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- MedSavantShared/src/org/ut/biolab/medsavant/model/Cohort.java
+++ MedSavantShared/src/org/ut/biolab/medsavant/model/Cohort.java
@@ -10,11 +10,11 @@ import java.io.Serializable;
  *
  * @author Andrew
  */
-public class Cohort implements Serializable {
-    
+public class Cohort implements Serializable, Comparable {
+
     private int id;
     private String name;
-    
+
     public Cohort(int id, String name){
         this.id = id;
         this.name = name;
@@ -27,9 +27,14 @@ public class Cohort implements Serializable {
     public String getName() {
         return name;
     }
-    
+
     @Override
     public String toString() {
         return getName();
+    }
+
+    @Override
+    public int compareTo(Object t) {
+        return this.getName().compareTo(t.toString());
     }
 }
