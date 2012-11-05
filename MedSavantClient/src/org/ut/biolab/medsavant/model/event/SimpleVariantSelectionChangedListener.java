3aa5ce124b1927012b2e02e61340206a643a8bc4
Marc Fiume <mfiume@cs.toronto.edu>
2012-11-05 20:54:49 +0000

    Updates to Inspector, preparation for moving away from static versions
    git-svn-id: https://svn2.assembla.com/svn/savant/MedSavant/trunk@3716 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- MedSavantClient/src/org/ut/biolab/medsavant/model/event/SimpleVariantSelectionChangedListener.java
+++
@@ -1,9 +0,0 @@
-package org.ut.biolab.medsavant.model.event;
-
-import org.ut.biolab.medsavant.view.genetics.inspector.VariantInspectorDialog;
-
-public interface SimpleVariantSelectionChangedListener {
-
-    public void simpleVariantSelectionChanged(VariantInspectorDialog.SimpleVariant r);
-
-}
