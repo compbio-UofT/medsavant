e259878f9dc3644a16c45487bdb135d245d4dcfb
Xenosky <bogdan.aurel.vancea@gmail.com>
2013-06-29 21:36:00 +0300

    Issue #19: Create an abstraction layer for managing JPQL queries Added an abstract implementation of the Query interface.

---
+++ MedSavantShared/src/main/java/org/ut/biolab/medsavant/shared/query/base/AbstractQuery.java
@@ -0,0 +1,112 @@
+package org.ut.biolab.medsavant.shared.query.base;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.ut.biolab.medsavant.shared.query.Query;
+import org.ut.biolab.medsavant.shared.query.QueryException;
+import org.ut.biolab.medsavant.shared.query.QueryExecutor;
+import org.ut.biolab.medsavant.shared.query.QuerySort;
+
+import java.util.*;
+
+/**
+ * Partial implementation for the query interface.
+ */
+public abstract class AbstractQuery implements Query {
+
+    /**
+     * The appropriate query executor for this
+     */
+    protected QueryExecutor executor;
+
+    /**
+     * The start of the result list in the result set.
+     */
+    protected int start;
+
+    /**
+     * The maximum number of results that can be returned.
+     */
+    protected int limit;
+
+    /**
+     * The map holding the query parameter values
+     */
+    protected Map<String, Object> parameters = new HashMap<String, Object>();
+
+    /**
+     * Array of sorts associated for this query.
+     */
+    protected QuerySort[] sorts;
+
+    @Override
+    public Query setParameter(String parameterName, Object value) {
+        //what to do if the parameter name is not found in the query?
+        parameters.put(parameterName, value);
+        return this;
+    }
+
+    @Override
+    public <T> List<T> execute() throws QueryException {
+        return executor.execute(this);
+    }
+
+    @Override
+    public Set<String> getParameterNames() {
+        return parameters.keySet();
+    }
+
+    @Override
+    public Collection<Object> getParameters() {
+        return parameters.values();
+    }
+
+    @Override
+    public Map<String, Object> getParameterMap() {
+        return parameters;
+    }
+
+    @Override
+    public Query setStart(int start) {
+        this.start = start;
+        return this;
+    }
+
+    @Override
+    public int getStart() {
+        return start;
+    }
+
+    @Override
+    public Query setLimit(int limit) {
+        this.limit = limit;
+        return this;
+    }
+
+    @Override
+    public int getLimit() {
+        return limit;
+    }
+
+    @Override
+    public Query setSorts(QuerySort[] sorts) {
+        this.sorts = sorts;
+        return this;
+    }
+
+    @Override
+    public QuerySort[] getSorts() {
+        return sorts;
+    }
+
+    @Override
+    public Query addSort(QuerySort sort) {
+        this.sorts = ArrayUtils.add(sorts, sort);
+        return this;
+    }
+
+    @Override
+    public Query addSorts(QuerySort[] newSorts) {
+        this.sorts = ArrayUtils.addAll(sorts, newSorts);
+        return this;
+    }
+}
