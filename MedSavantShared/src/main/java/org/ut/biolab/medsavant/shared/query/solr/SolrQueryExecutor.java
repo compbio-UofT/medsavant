2e1813439cad0838af60cdd9c564d69cf889d204
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2013-07-16 22:26:04 +0300

    [misc] Added license and cleaned up unused imports.

--- MedSavantShared/src/main/java/org/ut/biolab/medsavant/shared/query/solr/SolrQueryExecutor.java
+++ MedSavantShared/src/main/java/org/ut/biolab/medsavant/shared/query/solr/SolrQueryExecutor.java
@@ -1,14 +1,27 @@
+/*
+ *    Copyright 2011-2012 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package org.ut.biolab.medsavant.shared.query.solr;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.beans.DocumentObjectBinder;
 import org.apache.solr.common.SolrDocumentList;
 import org.ut.biolab.medsavant.shared.query.Query;
 import org.ut.biolab.medsavant.shared.query.QueryExecutor;
 import org.ut.biolab.medsavant.shared.query.ResultRow;
-import org.ut.biolab.medsavant.shared.query.SimpleSolrQuery;
 import org.ut.biolab.medsavant.shared.query.parser.JPQLToSolrTranslator;
 import org.ut.biolab.medsavant.shared.solr.exception.InitializationException;
 import org.ut.biolab.medsavant.shared.solr.mapper.MapperRegistry;
@@ -16,7 +29,6 @@ import org.ut.biolab.medsavant.shared.solr.mapper.ResultMapper;
 import org.ut.biolab.medsavant.shared.solr.service.AbstractSolrService;
 import org.ut.biolab.medsavant.shared.solr.service.SolrServiceRegistry;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
