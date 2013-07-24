7e6f31750ca8aac3bb839353f03f23fd9f07f614
mcupak <mcupak@redhat.com>
2013-07-25 00:37:58 +0200

    added connection controller for a sharded session

---
+++ MedSavantShared/src/main/java/org/ut/biolab/medsavant/shared/db/shard/ShardedConnectionController.java
@@ -0,0 +1,34 @@
+/*
+ *    Copyright 2011-2012 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.ut.biolab.medsavant.shared.db.shard;
+
+import org.hibernate.Session;
+
+/**
+ * Shards session controller.
+ * 
+ * @author <a href="mailto:mirocupak@gmail.com">Miroslav Cupak</a>
+ * 
+ */
+public class ShardedConnectionController {
+    public static Session openSession() {
+        return HibernateShardUtil.getSessionFactory().openSession();
+    }
+
+    public static void closeSession(Session session) {
+        session.close();
+    }
+}
