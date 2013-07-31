e46067d404a6f0ac6792b28f8b58f4c1dea46fff
jim <jim@w510.(none)>
2013-07-31 03:03:54 -0400

    - A few more files added

---
+++ MedSavantClient/src/main/java/org/ut/biolab/medsavant/client/view/genetics/variantinfo/OtherIndividualsGeneSubInspector.java
@@ -0,0 +1,195 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ut.biolab.medsavant.client.view.genetics.variantinfo;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.lang.String;
+import java.text.NumberFormat;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ut.biolab.medsavant.client.api.Listener;
+import org.ut.biolab.medsavant.client.filter.SearchBar;
+import org.ut.biolab.medsavant.client.view.SplitScreenPanel;
+import org.ut.biolab.medsavant.client.view.images.IconFactory;
+import org.ut.biolab.medsavant.client.view.util.ViewUtil;
+import org.ut.biolab.medsavant.shared.format.BasicPatientColumns;
+import org.ut.biolab.medsavant.shared.format.BasicVariantColumns;
+import org.ut.biolab.medsavant.shared.model.Cohort;
+import org.ut.biolab.medsavant.shared.model.Gene;
+import org.ut.biolab.medsavant.shared.model.GenomicRegion;
+import org.ut.biolab.medsavant.shared.vcf.VariantRecord;
+import org.ut.biolab.mfiume.query.QueryViewController;
+
+/**
+ *
+ * @author jim
+ */
+public class OtherIndividualsGeneSubInspector extends OtherIndividualsSubInspector implements Listener<Object> {
+
+    private static final Log LOG = LogFactory.getLog(OtherIndividualsGeneSubInspector.class);    
+    private static final int MIDDLE_LEVEL_INDENT = 10;    
+    private static final int MAX_GENENAME_LENGTH_TODISPLAY = 15;
+    private static final String[] COLUMN_NAMES_IN_AGGREGATE_TABLE = new String[]{"Position", "Ref.", "Alt.", BasicVariantColumns.ZYGOSITY.getAlias()};
+    private Gene currentGene;
+  
+    @Override
+    protected JPanel getIndividualSummaryPanel(String dnaID) {
+        JPanel outerPanel = new JPanel();
+        outerPanel.setLayout(new BoxLayout(outerPanel, BoxLayout.Y_AXIS));
+
+        Set<VariantRecord> variantRecords = getVariantRecords(dnaID);
+
+
+        Map<Long, Set<VariantRecord>> positionVariantMap = new TreeMap<Long, Set<VariantRecord>>();
+        for (VariantRecord variantRecord : variantRecords) {
+            Set<VariantRecord> variantsAtPosition = positionVariantMap.get(variantRecord.getPosition());
+            if (variantsAtPosition == null) {
+                variantsAtPosition = new HashSet<VariantRecord>();
+            }
+            variantsAtPosition.add(variantRecord);
+            positionVariantMap.put(variantRecord.getPosition(), variantsAtPosition);
+        }
+
+
+        for (Map.Entry<Long, Set<VariantRecord>> e : positionVariantMap.entrySet()) {
+            Long pos = e.getKey();
+            Set<VariantRecord> variantsAtPosition = e.getValue();
+            for (final VariantRecord variantRecord : variantsAtPosition) {
+                final JPanel rowContainer = new JPanel();
+                rowContainer.setLayout(new BoxLayout(rowContainer, BoxLayout.Y_AXIS));
+                final JPanel row = new JPanel();
+                row.setLayout(new BoxLayout(row, BoxLayout.X_AXIS));
+                row.setBorder(new EmptyBorder(0, MIDDLE_LEVEL_INDENT, 0, 0));
+                final JLabel showDetailsButton = ViewUtil.createIconButton(IconFactory.getInstance().getIcon(IconFactory.StandardIcon.EXPAND));
+                String posStr = NumberFormat.getNumberInstance().format(pos);
+                JLabel rowTitle = new JLabel(" "+variantRecord.getZygosity().name() + " " + variantRecord.getType() + " @ " + posStr);
+                row.add(showDetailsButton);
+                row.add(rowTitle);
+                row.add(Box.createHorizontalGlue());
+                rowContainer.add(row);
+                outerPanel.add(rowContainer);
+
+
+
+                showDetailsButton.addMouseListener(new MouseAdapter() {
+                    private boolean expanded = true;
+
+                    @Override
+                    public void mousePressed(MouseEvent me) {
+                        if (expanded) {
+                            showDetailsButton.setIcon(IconFactory.getInstance().getIcon(IconFactory.StandardIcon.COLLAPSE));
+                            int last_level_indent = MIDDLE_LEVEL_INDENT
+                                    + IconFactory.getInstance().getIcon(IconFactory.StandardIcon.EXPAND).getIconWidth()
+                                    + rowContainer.getFontMetrics(rowContainer.getFont()).charWidth(' ');
+                            JPanel p = new JPanel();
+                            p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+                            p.setBorder(new EmptyBorder(0, last_level_indent, 0, 0));
+                            JPanel textPanel = new JPanel();
+                            textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.X_AXIS));
+                            textPanel.add(new JLabel("Ref: " + variantRecord.getRef()));
+                            textPanel.add(Box.createHorizontalGlue());
+                            p.add(textPanel);
+
+                            textPanel = new JPanel();
+                            textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.X_AXIS));
+                            textPanel.add(new JLabel("Alt: " + variantRecord.getAlt()));
+                            textPanel.add(Box.createHorizontalGlue());
+                            p.add(textPanel);
+
+                            rowContainer.add(p);
+                        } else {
+                            showDetailsButton.setIcon(IconFactory.getInstance().getIcon(IconFactory.StandardIcon.EXPAND));
+                            rowContainer.removeAll();
+                            rowContainer.add(row);
+                        }
+                        rowContainer.revalidate();
+                        rowContainer.repaint();
+                        expanded = !expanded;
+                    }
+                });
+
+            }
+        }
+        return outerPanel;
+    }
+
+    @Override
+    public boolean setObject(Object obj) {
+        if (obj instanceof Gene) {
+            if(this.currentGene == (Gene) obj){
+                return false;
+            }else{
+                this.currentGene = (Gene) obj;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Note that this method executes in its own thread.
+    @Override
+    protected synchronized List<Object[]> getQueryResults() {
+        QueryViewController qvc = SearchBar.getInstance().getQueryViewController();
+        GenomicRegion gr = new GenomicRegion("", currentGene.getChrom(), currentGene.getStart(), currentGene.getEnd());
+        List<Object[]> results = qvc.restrictToRegion(gr, null, MAXIMIUM_VARIANTS_TO_FETCH);
+        //System.out.println("OtherIndividualsGeneSubInspector: Got " + results.size() + " results.");
+        return results;
+    }
+
+    @Override
+    public String getName() {
+        return "Individuals with a variant in this gene";
+    }
+        
+    public OtherIndividualsGeneSubInspector(SplitScreenPanel splitScreenPanel){
+        super(splitScreenPanel);
+        VariantFrequencyAggregatePane ap = new VariantFrequencyAggregatePane(COLUMN_NAMES_IN_AGGREGATE_TABLE) {
+            @Override
+            public Object[] getRow(Cohort cohort, String familyId, VariantRecord variantRecord) {
+                return new Object[]{
+                    variantRecord.getPosition(),
+                    variantRecord.getRef(),
+                    variantRecord.getAlt(),
+                    variantRecord.getZygosity()
+                };
+            }
+
+            @Override
+            public String getTitle(String currentFirstColumn) {
+                String geneName = "this gene";
+                if(currentGene != null){
+                    geneName = "gene "+currentGene.getName();
+                }
+                if(geneName.length() > MAX_GENENAME_LENGTH_TODISPLAY){
+                    geneName = "this gene";
+                }
+                
+                if (currentFirstColumn.equals(BasicPatientColumns.FAMILY_ID.getAlias())) {
+                    return "Variants within "+geneName+" by Family";
+                } else {
+                    return "Variants within "+geneName+" by Cohort";
+                }
+            }
+
+            @Override
+            public void selectVariant(VariantRecord variantRecord) {
+                OtherIndividualsGeneSubInspector.this.selectVariant(variantRecord);
+            }
+        };
+
+        init(ap);
+    }
+}
---
+++ MedSavantClient/src/main/java/org/ut/biolab/medsavant/client/view/genetics/variantinfo/OtherIndividualsVariantSubInspector.java
@@ -0,0 +1,175 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ut.biolab.medsavant.client.view.genetics.variantinfo;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Set;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import org.ut.biolab.medsavant.client.filter.SearchBar;
+import org.ut.biolab.medsavant.client.view.SplitScreenPanel;
+import static org.ut.biolab.medsavant.client.view.genetics.variantinfo.OtherIndividualsSubInspector.MAXIMIUM_VARIANTS_TO_FETCH;
+import org.ut.biolab.medsavant.shared.format.BasicPatientColumns;
+import org.ut.biolab.medsavant.shared.format.BasicVariantColumns;
+import org.ut.biolab.medsavant.shared.model.Cohort;
+import org.ut.biolab.medsavant.shared.vcf.VariantRecord;
+import org.ut.biolab.mfiume.query.QueryViewController;
+
+/**
+ *
+ * @author jim
+ */
+public class OtherIndividualsVariantSubInspector extends OtherIndividualsSubInspector {
+
+    private static final String[] COLUMN_NAMES_IN_AGGREGATE_TABLE = new String[]{"Ref.", "Alt.", BasicVariantColumns.ZYGOSITY.getAlias()};
+    private static final int MAX_POSITION_STRLENGTH_TODISPLAY = 30;
+    private SimpleVariant currentVariant;
+    public OtherIndividualsVariantSubInspector(SplitScreenPanel splitScreenPanel) {
+        super(splitScreenPanel);
+        VariantFrequencyAggregatePane ap = new VariantFrequencyAggregatePane(COLUMN_NAMES_IN_AGGREGATE_TABLE) {
+            @Override
+            public Object[] getRow(Cohort cohort, String familyId, VariantRecord variantRecord) {
+                return new Object[]{
+                    variantRecord.getRef(),
+                    variantRecord.getAlt(),
+                    variantRecord.getZygosity()
+                };
+            }
+
+            @Override
+            public String getTitle(String currentFirstColumn) {                
+                String position = "this position";
+                if(currentVariant != null){
+                    position = "Position "+NumberFormat.getNumberInstance().format(currentVariant.getGenomicRegion().getStart());
+                }
+                if(position.length() > MAX_POSITION_STRLENGTH_TODISPLAY){
+                    position = "this position";
+                }
+                
+                if (currentFirstColumn.equals(BasicPatientColumns.FAMILY_ID.getAlias())) {
+                    return "Variants at "+position+" by Family";
+                } else {
+                    return "Variants at "+position+" by Cohort";
+                }
+            }
+
+            @Override
+            public void selectVariant(VariantRecord variantRecord) {
+                OtherIndividualsVariantSubInspector.this.selectVariant(variantRecord);
+            }
+        };
+
+        init(ap);
+    }
+
+    @Override
+    public String getName() {
+        return "Individuals with a variant at this position";
+    }
+
+    @Override
+    protected JPanel getIndividualSummaryPanel(String dnaID){
+        return new VariantSummaryPanel(dnaID);
+    }
+    
+    @Override
+    public boolean setObject(Object obj) {
+        if (obj instanceof SimpleVariant) {
+            if(this.currentVariant == (SimpleVariant)obj){
+                return false;
+            }else{
+                this.currentVariant = (SimpleVariant) obj;            
+                return true;
+            }
+        }
+        return false;        
+    }
+    
+    @Override
+    protected synchronized List<Object[]> getQueryResults(){
+        QueryViewController qvc = SearchBar.getInstance().getQueryViewController();
+        List<Object[]> results = qvc.restrictToRegion(currentVariant.getGenomicRegion(), currentVariant.alt, MAXIMIUM_VARIANTS_TO_FETCH);        
+        return results;
+    }
+    
+    private class VariantSummaryPanel extends JPanel {
+
+        public VariantSummaryPanel(String dnaID) {
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+            //Map<String, Map<String, VariantRecord>> dnaIDVariantMap
+
+            //Map<String, VariantRecord> typeVariantMap = dnaIDVariantMap.get(dnaID);
+            Set<VariantRecord> variantRecords = dnaIDVariantMap.get(dnaID);
+
+            for (final VariantRecord variantRecord : variantRecords) {
+                int initialIndent = getFontMetrics(getFont()).charWidth('T');
+                int subsequentIndent = getFontMetrics(getFont()).stringWidth("Typ");
+                add(getRow("Type: " + variantRecord.getType(), initialIndent));
+                add(getRow("Zygosity: " + variantRecord.getZygosity().toString(), subsequentIndent));
+                add(getRow("Ref.: " + variantRecord.getRef(), subsequentIndent));
+                add(getRow("Alt.: " + variantRecord.getAlt(), subsequentIndent));
+
+                //add(getRow("Zygosity: ", e.getValue().getZygosity().toString(), "Alt.: ", e.getValue().getAlt()));
+                JButton showButton = new JButton("Show");
+                showButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent ae) {
+                        selectVariant(variantRecord);
+                    }
+                });
+
+                JPanel buttonPanel = new JPanel();
+                buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+                buttonPanel.add(Box.createHorizontalGlue());
+                buttonPanel.add(showButton);
+                add(buttonPanel);
+            }
+        }
+
+        private JPanel getRow(String txt, int indent) {
+            JPanel p = new JPanel();
+            p.setLayout(new BoxLayout(p, BoxLayout.X_AXIS));
+            p.setBorder(new EmptyBorder(0, indent, 0, 0));
+            p.add(getLabel(txt));
+            p.add(Box.createHorizontalGlue());
+            return p;
+        }
+
+        private JPanel getRow(String lbl1, String value1, String lbl2, String value2) {
+            JPanel p = new JPanel();
+            p.setLayout(new BoxLayout(p, BoxLayout.X_AXIS));
+            p.add(getLabel(lbl1));
+            p.add(getLabel(value1));
+            p.add(Box.createHorizontalGlue());
+            p.add(getLabel(lbl2));
+            p.add(getLabel(value2));
+            return p;
+        }
+
+        private JLabel getLabel(String txt) {
+            JLabel lbl = new JLabel(txt);
+            //lbl.setFont(lbl.getFont().deriveFont(Font.ITALIC));
+            lbl.setToolTipText("Foo");
+            lbl.setAlignmentX(1.0f);
+            lbl.setAlignmentY(1.0f);
+            return lbl;
+        }
+
+        private JLabel getBoldLabel(String txt) {
+            JLabel lbl = new JLabel(txt);
+            lbl.setFont(lbl.getFont().deriveFont(Font.BOLD));
+            return lbl;
+        }
+    }
+}
