dded5d692f7fa8f04339def6510f43ec7d447e13
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2013-08-28 14:03:12 +0300

    Issue #15: Add Solr indexing support for variants Propagated exceptions to RPC interface.

--- MedSavantShared/src/main/java/org/ut/biolab/medsavant/shared/serverapi/VariantManagerAdapter.java
+++ MedSavantShared/src/main/java/org/ut/biolab/medsavant/shared/serverapi/VariantManagerAdapter.java
@@ -37,6 +37,7 @@ import org.ut.biolab.medsavant.shared.model.SessionExpiredException;
 import org.ut.biolab.medsavant.shared.model.SimplePatient;
 import org.ut.biolab.medsavant.shared.model.SimpleVariantFile;
 import org.ut.biolab.medsavant.shared.model.VariantComment;
+import org.ut.biolab.medsavant.shared.solr.exception.InitializationException;
 
 
 /**
@@ -57,7 +58,7 @@ public interface VariantManagerAdapter extends Remote {
     public void cancelPublish(String sessID, int projID, int referenceID, int updateID) throws Exception;
     public int updateTable(String sessID, int projID, int refID, int[] annotIDs, CustomField[] variantFields, boolean autoPublish, String email) throws Exception;
     public int removeVariants(String sessID, int projID, int refID, List<SimpleVariantFile> files, boolean autoPublish, String email) throws Exception;
-    public int exportVariants(String sessID, int projID, int refID, Condition[][] conditions, boolean orderedByPosition, boolean zipped) throws SQLException, SessionExpiredException, RemoteException, IOException, InterruptedException;
+    public int exportVariants(String sessID, int projID, int refID, Condition[][] conditions, boolean orderedByPosition, boolean zipped) throws SQLException, SessionExpiredException, RemoteException, IOException, InterruptedException, InitializationException;
 
     public TableSchema getCustomTableSchema(String sessID, int projID, int refID) throws SQLException, RemoteException, SessionExpiredException;
     public List<Object[]> getVariants(String sessID,int projID, int refID, int start, int limit) throws SQLException, RemoteException, SessionExpiredException;
