95e47a5889dac86186ab81e97a4569b517ea7345
mcupak <mcupak@redhat.com>
2013-09-05 11:17:42 +0200

    issue #43: added tests for direct shard access and database/table manipulation

---
+++ medsavant-shard/src/test/java/org/ut/biolab/medsavant/shard/variant/WriteTest.java
@@ -0,0 +1,221 @@
+/*
+ *    Copyright 2011-2012 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/*
+ *    Copyright 2011-2012 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.ut.biolab.medsavant.shard.variant;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.testng.annotations.Test;
+import org.ut.biolab.medsavant.shard.db.DBUtil;
+import org.ut.biolab.medsavant.shard.nonshard.ShardConfigurationUtil;
+import org.ut.biolab.medsavant.shard.nonshard.ShardedConnectionController;
+
+/**
+ * Tests to verify DDL is working.
+ * 
+ * @author <a href="mailto:mirocupak@gmail.com">Miroslav Cupak</a>
+ * 
+ */
+public class WriteTest extends AbstractShardTest {
+
+    @Test
+    public void testShardUrl() {
+        Session session = ShardedSessionManager.openSession();
+
+        System.out.println(ShardConfigurationUtil.getConnectionUrlForShard(0));
+
+        ShardedSessionManager.closeSession(session);
+    }
+
+    @Test
+    public void testShardDb() {
+        Session session = ShardedSessionManager.openSession();
+
+        System.out.println(ShardConfigurationUtil.getDbForShard(0));
+
+        ShardedSessionManager.closeSession(session);
+    }
+
+    @Test
+    public void testShardServer() {
+        Session session = ShardedSessionManager.openSession();
+
+        System.out.println(ShardConfigurationUtil.getServerForShard(0));
+
+        ShardedSessionManager.closeSession(session);
+    }
+
+    @Test
+    public void testShardConnectionCredentials() {
+        Session session = ShardedSessionManager.openSession();
+
+        System.out.println(ShardedSessionManager.getConfig(0).getShardUser());
+        System.out.println(ShardedSessionManager.getConfig(0).getShardPassword());
+
+        ShardedSessionManager.closeSession(session);
+    }
+
+    @Test
+    public void testExportTableOnOneShard() {
+        final String query = "SELECT * FROM z_variant_proj1_ref3_update1 LIMIT 10 INTO OUTFILE '/tmp/test.bu' FIELDS TERMINATED BY '\t'";
+
+        Session session = ShardedSessionManager.openSession();
+
+        ShardedConnectionController.executeQueryWithoutResultOnShard(0, query);
+        BufferedReader br = null;
+        String everything = null;
+        try {
+            br = new BufferedReader(new FileReader("/tmp/test.bu"));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append('\n');
+                line = br.readLine();
+            }
+            everything = sb.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+                File file = new File("/tmp/test.bu");
+                if (file.delete()) {
+                    System.out.println(file.getName() + " is deleted!");
+                } else {
+                    System.out.println("Delete operation is failed.");
+                }
+            } catch (IOException e) {
+                System.err.println("I/O error occured.");
+            }
+        }
+
+        ShardedSessionManager.closeSession(session);
+
+        System.out.println(everything);
+    }
+
+    @Test
+    public void testExportTableOnAllShards() {
+        final String query = "SELECT * FROM z_variant_proj1_ref3_update1 LIMIT 10 INTO OUTFILE '/tmp/test-%d.bu' FIELDS TERMINATED BY '\t'";
+
+        Session session = ShardedSessionManager.openSession();
+
+        ShardedConnectionController.executeQueryWithoutResultOnAllShards(query, true);
+
+        // read the files
+        BufferedReader br = null;
+        String everything = null;
+        for (int i = 0; i < ShardedSessionManager.getShardNo(); i++) {
+            try {
+                br = new BufferedReader(new FileReader("/tmp/test-" + i + ".bu"));
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
+
+                while (line != null) {
+                    sb.append(line);
+                    sb.append('\n');
+                    line = br.readLine();
+                }
+                everything = sb.toString();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    br.close();
+                    File file = new File("/tmp/test-" + i + ".bu");
+                    if (file.delete()) {
+                        System.out.println(file.getName() + " is deleted!");
+                    } else {
+                        System.out.println("Delete operation is failed.");
+                    }
+                } catch (IOException e) {
+                    System.err.println("I/O error occured.");
+                }
+            }
+        }
+
+        ShardedSessionManager.closeSession(session);
+
+        System.out.println(everything);
+    }
+
+    @Test
+    public void testCreateDropShards() {
+        // show databases before shards
+        System.out.println("Databases before creating shards:");
+        showDatabases();
+
+        // create shards
+        System.out.println("Creating shards...");
+        Session session = ShardedSessionManager.openSession();
+        ShardedConnectionController.createShards();
+        ShardedSessionManager.closeSession(session);
+
+        // show databases after creating shards
+        System.out.println("Databases after creating shards:");
+        showDatabases();
+
+        // drop shards
+        System.out.println("Dropping shards...");
+        session = ShardedSessionManager.openSession();
+        ShardedConnectionController.dropShards();
+        ShardedSessionManager.closeSession(session);
+
+        // show databases after dropping shards
+        System.out.println("Databases after dropping shards:");
+        showDatabases();
+    }
+
+    @Test
+    public void showDatabases() {
+        Session session = ShardedSessionManager.openSession();
+
+        List<String> dbs = DBUtil.getDatabases(ShardConfigurationUtil.getServerForShard(0), ShardedSessionManager.getConfig(0).getShardUser(), ShardedSessionManager.getConfig(0)
+                .getShardPassword());
+
+        for (String s : dbs) {
+            System.out.println(s);
+        }
+
+        ShardedSessionManager.closeSession(session);
+    }
+}
