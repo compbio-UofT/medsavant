914943875cebd43303dc0261620c0b00293c9c73
mcupak <mcupak@redhat.com>
2013-09-09 12:53:00 +0200

    issue #43: fixed a broken commit

---
+++ medsavant-shard/src/main/java/org/ut/biolab/medsavant/shard/db/NonShardDBUtils.java
@@ -0,0 +1,150 @@
+/*
+ *    Copyright 2011-2012 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/*
+ *    Copyright 2011-2012 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.ut.biolab.medsavant.shard.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Helpers for direct database access.
+ * 
+ * @author <a href="mailto:mirocupak@gmail.com">Miroslav Cupak</a>
+ * 
+ */
+public class NonShardDBUtils {
+
+    private static final String SHOW_DB_QUERY = "SHOW DATABASES";
+    private static final String SHOW_TABLES_QUERY = "SHOW TABLES";
+    public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+    /**
+     * Retrieve the list of databases on the given server.
+     * 
+     * @param url
+     *            connection URL to the server
+     * @param user
+     *            user name
+     * @param password
+     *            password
+     * @return list of databases
+     */
+    public static List<String> getDatabases(String url, String user, String password) {
+        List<String> res = new ArrayList<String>();
+        PreparedStatement s = null;
+        ResultSet r = null;
+
+        try {
+            Class.forName(JDBC_DRIVER);
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            s = conn.prepareStatement(SHOW_DB_QUERY);
+            r = s.executeQuery();
+
+            while (r.next()) {
+                res.add(r.getString(1));
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("Driver not loaded: " + JDBC_DRIVER);
+        } catch (SQLException e) {
+            System.err.println("Error connecting to: " + url);
+        } finally {
+            if (s != null) {
+                try {
+                    s.close();
+                } catch (SQLException e) {
+                    System.err.println("Failed to close the statement.");
+                }
+            }
+            if (r != null) {
+                try {
+                    r.close();
+                } catch (SQLException e) {
+                    System.err.println("Resultset could not be closed.");
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Retrieves the list of tables in the given database.
+     * 
+     * @param url
+     *            database connection URL
+     * @param user
+     *            user name
+     * @param password
+     *            password
+     * @return list of tables
+     */
+    public static List<String> getTables(String url, String user, String password) {
+        List<String> res = new ArrayList<String>();
+        PreparedStatement s = null;
+        ResultSet r = null;
+
+        try {
+            Class.forName(JDBC_DRIVER);
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            s = conn.prepareStatement(SHOW_TABLES_QUERY);
+            r = s.executeQuery();
+
+            while (r.next()) {
+                res.add(r.getString(1));
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("Driver not loaded: " + JDBC_DRIVER);
+        } catch (SQLException e) {
+            System.err.println("Error connecting to: " + url);
+        } finally {
+            if (s != null) {
+                try {
+                    s.close();
+                } catch (SQLException e) {
+                    System.err.println("Failed to close the statement.");
+                }
+            }
+            if (r != null) {
+                try {
+                    r.close();
+                } catch (SQLException e) {
+                    System.err.println("Resultset could not be closed.");
+                }
+            }
+        }
+        return res;
+    }
+}
