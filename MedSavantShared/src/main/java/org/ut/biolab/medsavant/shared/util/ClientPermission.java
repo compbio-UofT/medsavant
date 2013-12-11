349d8c457d63566bfcb26ff9b4123ebf7ff532e4
jim <jim@w510.(none)>
2013-12-11 18:36:37 -0500

    Added some missing files

---
+++ MedSavantShared/src/main/java/org/ut/biolab/medsavant/shared/util/ClientPermission.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ut.biolab.medsavant.shared.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface ClientPermission {
+    public ClientType deny();
+}
