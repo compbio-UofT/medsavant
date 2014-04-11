64454943ebe1cf2afb92aaf7da69c2a609be3a78
Jim <jim@wodaklab.org>
2014-04-11 18:34:59 -0400

    - Converted/modified Mendel, Discovery, and Secondary findings apps to maven. - Fixed error with app loading code by introducing semaphore to wait for all apps to load before showing the dashboard.  Otherwise some apps would not show.

---
+++ MedSavantPlugins/medsavant.secondary/src/main/java/medsavant/secondary/SecondaryApp.java
@@ -0,0 +1,47 @@
+package medsavant.secondary;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import org.ut.biolab.medsavant.shared.appapi.MedSavantDashboardApp;
+
+
+public class SecondaryApp extends MedSavantDashboardApp {
+
+    private static final String iconroot= "/medsavant/secondary/icon/";
+    
+    private SecondaryPanel p;
+
+    @Override
+    public JPanel getContent() {
+        if (p  == null) {
+            p = new SecondaryPanel();
+        }
+        return p.getView();
+    }
+
+    /**
+     * Title which will appear on plugin's tab in Savant user interface.
+     */
+    @Override
+    public String getTitle() {               
+        return "Secondary";
+    }
+
+    @Override
+    public void viewDidLoad() {        
+    }
+
+    @Override
+    public void viewDidUnload() {                
+
+    }
+
+    @Override
+    public ImageIcon getIcon() {        
+        return getIcon(iconroot + "icon.png");
+    }
+
+    public ImageIcon getIcon(String resourcePath) {
+        return new ImageIcon(getClass().getResource(resourcePath));
+    }
+}
