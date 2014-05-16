4355de026e8b340a2a7956010d6604de9d2db01d
Jim <jim@wodaklab.org>
2014-05-16 17:22:12 -0400

    - Fixed some apps, ensured mavenized apps are updated to 1.3. - Added skeleton maven app to encourage app builders to use maven.  To write an app, copy the skeleton and edit the pom.xml according to the comments.

--- MedSavantPlugins/medsavant.cancer/src/main/java/medsavant/uhn/cancer/SetCommentStatusDialog.java
+++ MedSavantPlugins/medsavant.cancer/src/main/java/medsavant/uhn/cancer/SetCommentStatusDialog.java
@@ -34,7 +34,7 @@ import javax.swing.JTextArea;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.ut.biolab.medsavant.MedSavantClient;
-import org.ut.biolab.medsavant.client.login.LoginController;
+import org.ut.biolab.medsavant.client.view.login.LoginController;
 import org.ut.biolab.medsavant.shared.model.UserComment;
 import org.ut.biolab.medsavant.shared.model.UserCommentGroup;
 import org.ut.biolab.medsavant.shared.model.SessionExpiredException;
