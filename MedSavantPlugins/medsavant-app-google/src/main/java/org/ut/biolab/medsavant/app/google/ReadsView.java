6fe9fe4b14a8515c6ec630bd1c1a29cb48b50c5f
mfiume <marcfiume@gmail.com>
2014-07-03 17:12:31 -0400

    More improvements to the Google Genomics app

--- MedSavantPlugins/medsavant-app-google/src/main/java/org/ut/biolab/medsavant/app/google/ReadsView.java
+++ MedSavantPlugins/medsavant-app-google/src/main/java/org/ut/biolab/medsavant/app/google/ReadsView.java
@@ -14,44 +14,49 @@
 package org.ut.biolab.medsavant.app.google;
 
 import com.google.api.services.genomics.Genomics;
-import com.google.api.services.genomics.Genomics.Datasets;
-import com.google.api.services.genomics.Genomics.Datasets.List;
-import com.google.api.services.genomics.model.Dataset;
-import com.google.api.services.genomics.model.ListDatasetsResponse;
 import com.google.api.services.genomics.model.Readset;
 import com.google.api.services.genomics.model.SearchReadsetsRequest;
 import com.google.api.services.genomics.model.SearchReadsetsResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.security.GeneralSecurityException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.Writer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
-import javax.swing.SwingWorker;
 import net.miginfocom.swing.MigLayout;
 import org.ut.biolab.medsavant.app.google.original.GoogleAuthenticate;
 import org.ut.biolab.medsavant.app.google.original.GoogleBAMDataSource;
+import org.ut.biolab.medsavant.client.settings.DirectorySettings;
 import org.ut.biolab.medsavant.client.view.MedSavantFrame;
 import org.ut.biolab.medsavant.client.view.app.AppDirectory;
 import org.ut.biolab.medsavant.client.view.app.builtin.SavantApp;
 import org.ut.biolab.medsavant.client.view.component.WaitPanel;
 import org.ut.biolab.medsavant.client.view.util.StandardFixableWidthAppPanel;
 import org.ut.biolab.medsavant.client.view.util.ViewUtil;
+import org.ut.biolab.medsavant.client.view.util.form.NiceForm;
+import org.ut.biolab.medsavant.client.view.util.form.NiceFormField;
+import org.ut.biolab.medsavant.client.view.util.form.NiceFormFieldGroup;
+import org.ut.biolab.medsavant.client.view.util.form.NiceFormModel;
 import org.ut.biolab.medsavant.client.view.util.list.NiceList;
 import org.ut.biolab.medsavant.client.view.util.list.NiceListItem;
 import org.ut.biolab.medsavant.client.view.util.list.WhiteNiceListColorScheme;
-import savant.api.data.DataFormat;
+import org.ut.biolab.medsavant.component.field.editable.StringEditableField;
 import savant.api.util.DialogUtils;
 
 /**
@@ -60,11 +65,24 @@ import savant.api.util.DialogUtils;
  */
 class ReadsView extends JPanel {
 
+    private File datasetFile;
+
+    {
+        File googleDir = new File(DirectorySettings.getCacheDirectory(), "google");
+        googleDir.mkdirs();
+        datasetFile = new File(googleDir, "datasets.ser");
+    }
+
     private Genomics genomics;
     private JPanel datasetBlock;
 
     private GoogleDataset dataset = null;
     private Thread fetcher;
+
+    private boolean isUpdatingChooser = false;
+
+    private java.util.List<GoogleDataset> datasets;
+    private JComboBox datasetChooser;
 
     void updateGenomicsService() {
         this.removeAll();
@@ -84,45 +102,131 @@ class ReadsView extends JPanel {
             ex.printStackTrace();
         }
 
-        JPanel datasetChooserBlock = p.addBlock("Dataset");
+        JPanel datasetChooserBlock = p.addBlock();
 
         if (successfullyAuthenticated) {
 
-            datasetChooserBlock.setLayout(new MigLayout("insets 0, fillx, filly, wrap"));
+            datasetChooserBlock.setLayout(new MigLayout("insets 0, fillx, filly"));
 
-            GoogleDataset defaultDataset = new GoogleDataset("461916304629", "Simons Foundation");
-
-            final JComboBox datasetChooser = new JComboBox();
-            datasetChooser.addItem(defaultDataset);
-            datasetChooser.addItem(new GoogleDataset("383928317087", "PGP"));
-            datasetChooser.addItem(new GoogleDataset("376902546192", "1000 Genomes"));
-
-            datasetChooser.setSelectedItem(defaultDataset);
+            datasetChooser = new JComboBox();
+            loadDatasets();
+            updateChooser();
 
             datasetChooser.addActionListener(new ActionListener() {
 
                 @Override
                 public void actionPerformed(ActionEvent e) {
+                    if (isUpdatingChooser) {
+                        return;
+                    }
                     dataset = (GoogleDataset) datasetChooser.getSelectedItem();
                     try {
                         refreshReadsets();
-                    } catch (IOException ex) {
+                    } catch (Exception ex) {
                         ex.printStackTrace();
                     }
                 }
 
             });
 
+            JButton addButton = ViewUtil.getSoftButton("Add");
+            JButton removeButton = ViewUtil.getSoftButton("Remove");
+
+            ViewUtil.positionButtonFirst(addButton);
+            ViewUtil.positionButtonLast(removeButton);
+
+            addButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                    final JDialog dialog = new JDialog(MedSavantFrame.getInstance(), "Add Dataset", true);
+
+                    JPanel form = new JPanel(new MigLayout("wrap 2"));
+
+                    final StringEditableField nameField = new StringEditableField();
+                    nameField.setName("Name");
+                    nameField.setAutonomousEditingEnabled(false);
+                    nameField.setEditing(true);
+                    
+                    final StringEditableField idField = new StringEditableField();
+                    idField.setName("ID");
+                    idField.setAutonomousEditingEnabled(false);
+                    idField.setEditing(true);
+                    
+                    form.add(new JLabel("Name:"));
+                    form.add(nameField);
+                    form.add(new JLabel("ID:"));
+                    form.add(idField);
+
+                    JButton addButton = new JButton("OK");
+                    addButton.addActionListener(new ActionListener() {
+
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            
+                            String name = nameField.getValue();
+                            String id = idField.getValue();
+                            
+                            if (name.isEmpty() || id.isEmpty()) {
+                                DialogUtils.displayError("Name and ID must not be empty");
+                                return;
+                            }
+                            
+                            GoogleDataset ds = new GoogleDataset(id,name);
+                            datasets.add(ds);
+                            saveDatasets();
+                            dataset = ds;
+                            updateChooser();
+                            refreshReadsets();
+                            
+                            dialog.dispose();
+                        }
+
+                    });
+
+                    JPanel p = new JPanel(new MigLayout("wrap"));
+                    p.add(form);
+                    p.add(addButton);
+
+                    dialog.add(p);
+                    dialog.pack();
+
+                    dialog.setLocationRelativeTo(MedSavantFrame.getInstance());
+                    dialog.setVisible(true);
+                }
+
+            });
+
+            removeButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String name = dataset.getName();
+                    if (DialogUtils.askYesNo("<html>Are you sure you want to remove <i>" + name + "</i>?</html>") == DialogUtils.YES) {
+                        datasets.remove(dataset);
+                        saveDatasets();
+                        dataset = datasets.get(0);
+                        updateChooser();
+                        refreshReadsets();
+                    }
+                }
+
+            });
+
+            datasetChooserBlock.add(new JLabel("<html><b>Dataset:</b></html>"), "split");
             datasetChooserBlock.add(datasetChooser);
+            datasetChooserBlock.add(addButton, "gapx 0");
+            datasetChooserBlock.add(removeButton, "wrap, gapx 0");
 
             datasetBlock = ViewUtil.getClearPanel();
             datasetBlock.setLayout(new MigLayout("insets 0, fillx, filly"));
             datasetChooserBlock.add(datasetBlock, "growx 1.0, width 100%");
 
             try {
-                dataset = defaultDataset;
+                dataset = datasets.get(0);
                 refreshReadsets();
-            } catch (IOException ex) {
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
         } else {
@@ -130,6 +234,67 @@ class ReadsView extends JPanel {
         }
 
         this.add(p, BorderLayout.CENTER);
+    }
+
+    private void loadDatasets() {
+
+        if (!datasetFile.exists()) {
+            datasets = new ArrayList<GoogleDataset>();
+            datasets.add(new GoogleDataset("461916304629", "Simons Foundation"));
+            datasets.add(new GoogleDataset("383928317087", "PGP"));
+            datasets.add(new GoogleDataset("376902546192", "1000 Genomes"));
+            saveDatasets();
+
+        } else {
+            System.out.println("Loading datasets");
+            Reader reader = null;
+            try {
+                reader = new InputStreamReader(new FileInputStream(datasetFile), "UTF-8");
+                Gson gson = new GsonBuilder().create();
+                GoogleDataset[] datasetArray = gson.fromJson(reader, GoogleDataset[].class);
+                datasets = new ArrayList<GoogleDataset>(Arrays.asList(datasetArray));
+
+            } catch (Exception ex) {
+                Logger.getLogger(ReadsView.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    reader.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(ReadsView.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    private void saveDatasets() {
+        System.out.println("Saving datasets");
+        Writer writer = null;
+        try {
+            writer = new FileWriter(datasetFile);
+            Gson gson = new GsonBuilder().create();
+            gson.toJson(datasets, writer);
+            writer.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ReadsView.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException ex) {
+                Logger.getLogger(ReadsView.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private void updateChooser() {
+        isUpdatingChooser = true;
+        datasetChooser.removeAllItems();
+        for (GoogleDataset gd : datasets) {
+            datasetChooser.addItem(gd);
+        }
+        if (dataset != null && datasets.contains(dataset)) {
+            datasetChooser.setSelectedItem(dataset);
+        }
+        isUpdatingChooser = false;
     }
 
     private class GoogleDataset {
@@ -160,7 +325,7 @@ class ReadsView extends JPanel {
         this.updateGenomicsService();
     }
 
-    private void refreshReadsets() throws IOException {
+    private void refreshReadsets() {
 
         datasetBlock.removeAll();
 
@@ -210,7 +375,6 @@ class ReadsView extends JPanel {
                         } else {
                             for (Readset rs : readsets) {
                                 NiceListItem item = new NiceListItem(rs.getName(), rs.getId());
-                                //item.setDescription(rs.getId());
                                 niceList.addItem(item);
                             }
 
@@ -233,7 +397,7 @@ class ReadsView extends JPanel {
                         public void actionPerformed(ActionEvent e) {
                             try {
                                 refreshReadsets();
-                            } catch (IOException ex) {
+                            } catch (Exception ex) {
                                 ex.printStackTrace();
                             }
                         }
@@ -278,7 +442,7 @@ class ReadsView extends JPanel {
                             datasetBlock.removeAll();
                             datasetBlock.add(niceList.getSearchBar(), "width 100%, wrap");
                             datasetBlock.add(ViewUtil.getClearBorderlessScrollPane(niceList), "width 100%, height 500, growy 1.0, wrap");
-                            datasetBlock.add(b, "split");
+                            //datasetBlock.add(b, "split");
                             datasetBlock.add(load, "wrap, right");
                             datasetBlock.updateUI();
                         }
